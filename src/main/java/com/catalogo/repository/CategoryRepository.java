c7c838bc423de0f181e42c480e027e09cde382d3
Bruno Augusto de Almeida e Silva <brunoaugustosilva.ti@gmail.com>
2024-11-15 10:14:03 -0300

    initial domain and repository

---
+++ src/main/java/com/catalogo/repository/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.catalogo.repository;
+
+import com.catalogo.domain.category.Category;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends MongoRepository<Category, String> {
+}
---
+++ src/main/java/com/catalogo/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.catalogo.repository;
+
+import com.catalogo.domain.product.Product;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends MongoRepository<Product, String> {
+}
